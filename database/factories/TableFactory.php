ce6595702022bff003610ceb4ec8bc06a06c0d8d
adam <adam@pop-os.localdomain>
2019-04-17 14:45:28 +1200

    finished the test units for the areaController with Bilal

--- database/factories/TableFactory.php
+++ database/factories/TableFactory.php
@@ -5,6 +5,6 @@ use App\Table;
 $factory->define(Table::class, function (Faker $faker) {
     return [
         "area_id"=>1,
-        'token'=> str_random(25),
+        'token'=> str_random(25), 
     ];
 });
---
+++ tests/Unit/AreaControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Unit;
+use App\Area;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use DB;
+use Illuminate\Support\Facades\Log;
+
+class AreaControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    
+    private $ID;
+    public function testCreateArea()
+    {
+        $data = [
+            'level_id' => 1,
+            'name' => 'area 1',
+        ];
+        
+        $area = factory(Area::class)->create();
+        $response = $this->json('POST', '/areas/create',$data);
+        $response->assertStatus(200);
+        $response->assertJson(['status' => true]);
+        $response->assertJson(['data' => $data]);
+       
+        $this->ID=DB::table('areas')->orderBy('id','desc')->first()->id;
+
+        $this->assertTrue(true);
+    }
+
+    
+    public function testTotalTables(){
+        $this->ID=DB::table('areas')->orderBy('id','desc')->first()->id;
+
+        $request = [
+            'id' => $this->ID,
+        ];
+        $response = $this->json('POST', '/count',$request);
+        
+        $count=Area::find($this->ID)->tables()->count();
+        $this->assertJson($count=='count');
+
+    }
+
+    public function testTotalTakenTables(){
+        $this->ID=DB::table('areas')->orderBy('id','desc')->first()->id;
+
+        $request = [
+            'id' => $this->ID,
+        ];
+        $response = $this->json('POST', '/countTaken',$request);
+        
+        $count=Area::find($this->ID)->tables()->where('taken','=',1)->count();
+        $this->assertJson($count=='count');
+
+    }
+
+    public function testDeleteArea(){
+        $this->ID=DB::table('areas')->orderBy('id','desc')->first()->id;
+
+        $request = [
+            'id' => $this->ID,
+        ];
+        $area = factory(Area::class)->create();
+        $response = $this->json('POST', '/deleteArea',$request);
+        $checkArea=DB::table('areas')->where('id', '=', $request['id'])->count();
+        $this->assertTrue($checkArea==0);
+        $response->assertStatus(200);
+        $response->assertJson(['status' => true]);
+        
+    }
+    
+
+
+}
